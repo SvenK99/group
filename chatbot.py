d41b33176b1bb5ba489b0dcfa6bfeb077a7e3c83
Sven <sven.feiyu@gmail.com>
2025-04-01 14:49:52 +0800

    Add chatbot.py and update necessary files-1

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# Flask route for webhook1
+# Flask route for webhook2
 @app.route('/webhook', methods=['POST'])
 def webhook():
     global updater, dispatcher
